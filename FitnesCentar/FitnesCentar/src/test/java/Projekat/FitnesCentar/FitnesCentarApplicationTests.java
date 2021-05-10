c8915aad27f38d9b63216350dbddf44b39eb8fa6
madics <sergejmadic@gmail.com>
2021-05-10 16:56:00 +0200

    Inicijalni commit

---
+++ FitnesCentar/FitnesCentar/src/test/java/Projekat/FitnesCentar/FitnesCentarApplicationTests.java
@@ -0,0 +1,13 @@
+package Projekat.FitnesCentar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesCentarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
